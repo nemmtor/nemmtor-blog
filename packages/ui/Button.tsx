9cc079aa7a51ffce01f0e84d851e7c12916fda9d
nemmtor <kacper0witas@gmail.com>
2022-05-20 22:30:10 +0200

    feat: add tailwind

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,3 +1,7 @@
 export const Button = () => {
-  return <button type="button">elo</button>;
+  return (
+    <button type="button" className="bg-blue-500 hover:bg-blue-700 h-16 w-16">
+      elo
+    </button>
+  );
 };
