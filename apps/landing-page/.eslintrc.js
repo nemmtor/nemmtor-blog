9f48d93b692c225ac6991bd53c8d015485fa746e
nemmtor <kacper0witas@gmail.com>
2022-05-20 22:29:58 +0200

    refactor: move eslint around

--- apps/landing-page/.eslintrc.js
+++ apps/landing-page/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ...require('eslint-config/eslint-next.js'),
+  ...require('eslint-config-custom/eslint-next.js'),
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: './tsconfig.json',
