3c676fd044285b862b8ef171176bd18fef223ae4
nemmtor <kacper0witas@gmail.com>
2022-05-31 22:47:41 +0200

    feat: setup docker for strapi

--- apps/cms/config/server.js
+++ apps/cms/config/server.js
@@ -2,6 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array(
+      'APP_KEYS',
+      ['e2swPte3agft0A==','LQUHnAEk+VG4Cg==','62c3mv+MUiajWg==','r4/kueLsKK5ZgQ==']
+    ),
   },
 });
