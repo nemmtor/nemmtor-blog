9fba59c7e4ca02afa13a0685ef019b55fd355426
nemmtor <kacper0witas@gmail.com>
2022-05-31 22:47:41 +0200

    feat: setup docker for strapi

--- apps/cms/config/admin.js
+++ apps/cms/config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'm+J6q2f6koVrHg=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'i5bf3FXPT4N93Q=='),
   },
 });
