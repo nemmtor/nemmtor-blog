859b80444c142cff0412cb032066ca6b60e83243
nemmtor <kacper0witas@gmail.com>
2022-06-02 23:00:08 +0200

    feat: check ui regression 2

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -4,7 +4,7 @@ export const Button = ({ children, onClick }: ButtonProps) => {
   return (
     <button
       type="button"
-      className="bg-blue-500 hover:bg-blue-700 h-8 w-16"
+      className="bg-red-500 hover:bg-blue-700 h-16 w-16"
       onClick={onClick}
     >
       {children}
