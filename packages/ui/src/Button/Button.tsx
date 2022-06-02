a95352d1d59ef7301690dcde726df8f7eacf22cb
nemmtor <kacper0witas@gmail.com>
2022-06-02 21:26:32 +0200

    fix: storybook

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -4,7 +4,7 @@ export const Button = ({ children, onClick }: ButtonProps) => {
   return (
     <button
       type="button"
-      className="bg-blue-500 hover:bg-blue-700 h-16 w-16"
+      className="bg-blue-500 hover:bg-blue-700 h-32 w-16"
       onClick={onClick}
     >
       {children}
